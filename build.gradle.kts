481ab3d58484ba9484c297a86272b8c1bfb46522
Alexander Bogovich <bogovich.alexander@gmail.com>
2018-05-19 20:55:03 +0700

    make kts build script

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    groovy
+    `maven-publish`
+    `java-gradle-plugin`
+    `kotlin-dsl`
+}
+
+group   = "com.github.alexbogovich"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+}
+
+gradlePlugin {
+    (plugins) {
+        "greetingPlugin" {
+            id = "com.github.alexbogovich.hello-plugin"
+            implementationClass = "com.github.alexbogovich.plugin.greeting.GreetingPlugin"
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "standalone-hello-plugin"
+enableFeaturePreview("STABLE_PUBLISHING")
